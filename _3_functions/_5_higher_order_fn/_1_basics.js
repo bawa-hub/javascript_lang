842d9d76f5f836b3d003790b658f8465d06f6204
Vikram kumar <vikram.kumar@squareboat.com>
2023-07-06 16:09:25 +0530

    links modified

--- _3_functions/_5_higher_order_fn/_1_basics.js
+++ _3_functions/_5_higher_order_fn/_1_basics.js
@@ -1,1 +1,61 @@
-// a function that takes another function as argument or return a function is called higher order function.
+// functions that take one or more functions as arguments, or return a function as their result.
+
+/*************************************** Without using HOF *****************/
+
+const radius = [1, 2, 3];
+
+// function to calculate area of the circle
+const calculateArea = function (radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(Math.PI * radius[i] * radius[i]);
+  }
+  return output;
+};
+
+// function to calculate diameter of the circle
+const calculateDiameter = function (radius) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * radius[i]);
+  }
+  return output;
+};
+console.log(calculateArea(radius));
+console.log(calculateDiameter(radius));
+
+/************************************** using HOF *****************/
+// logic to clculate area [CALLBACK FUNCTION]
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+// logic to calculate diameter [CALLBACK FUNCTION]
+const diameter = function (radius) {
+  return 2 * radius;
+};
+
+// [CALLBACK FUNCTION]
+const circumeference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+
+// a reusable function to calculate area, diameter, etc [HIGHER ORDER FUNCTION]
+const calculate = function (radius, logic) {
+  const output = [];
+  for (let i = 0; i < radius.length; i++) {
+    output.push(logic(radius[i]));
+  }
+  return output;
+};
+
+console.log(calculate(radius, area));
+console.log(calculate(radius, diameter));
+console.log(calculate(radius, circumeference));
+
+// How to Use Higher Order Functions
+
+// You can use higher order functions in a variety of ways.
+// When working with arrays, you can use the map(), reduce(), filter(), and sort() functions to manipulate and transform data in an array.
+// When working with objects, you can use the Object.entries() function to create a new array from an object.
+// When working with functions, you can use the compose() function to create complex functions from simpler ones.
